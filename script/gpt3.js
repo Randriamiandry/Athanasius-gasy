01d13709b0f9d1380435c58f15a6fd3c3d5a5643
Randriamiandry <randriamiandryathanasios@gmail.com>
2024-08-30 17:39:24 +0300

    Update gpt3.js

--- script/gpt3.js
+++ script/gpt3.js
@@ -37,7 +37,7 @@ module.exports.run = async function ({ api, event, args }) {
             const generatedText = response.data.response;
 
             // Ai Answer Here
-            api.sendMessage(`GPT 3.5 Turbo. \n━━━━━━━━━━━━━━━━━━\n${generatedText}\n━━━━━━━━━━━━━━━━━━`, event.threadID, messageID);
+            api.sendMessage(`༆🅖︎🅟︎🅣︎ 3.5 🅣︎🅤︎🅡︎🅑︎🅞︎༆. \n━━━━━━━━━━━━━━━━━━\n${generatedText}\n━━━━━━━━━━━━━━━━━━`, event.threadID, messageID);
         } else {
             console.error('API response did not contain expected data:', response.data);
             api.sendMessage(`❌ An error occurred while generating the text response. Please try again later. Response data: ${JSON.stringify(response.data)}`, event.threadID, messageID);

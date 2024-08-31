3b3755ead8c1c14e629827d36ff50b084fc7407b
Randriamiandry <randriamiandryathanasios@gmail.com>
2024-08-31 13:07:34 +0300

    Update gpt4.js

--- script/gpt4.js
+++ script/gpt4.js
@@ -1,39 +1,27 @@
-const { Hercai } = require('hercai');
-const herc = new Hercai();
+const { RsnChat } = require("rsnchat");
+
+const rsnchat = new RsnChat("rsnai_q6XwhE3xUtjjW3SwxiO5xq9o");
 
 module.exports.config = {
-  name: 'gpt4',
-  version: '1.1.0',
-  hasPermssion: 0,
-  credits: 'oliver gab| Liane Cagara',
-  description: 'An AI command using Hercai API!',
-  hasPrefix: false,
-  allowPrefix: true,
-  commandCategory: 'chatbots',
-  usages: 'Ai [prompt]',
-  cooldowns: 5,
+		name: "gpt4",
+		version: "1.0.0",
+		role: 0,
+	  aliases: ["Gpt4"],
+		credits: "cliff",
+		hasPrefix: false,
+		description: "Ask GPT-4 a question.",
+		usage: "<question>",
+		cooldowns: 5
 };
 
-module.exports.run = async function ({ api, event, args, box }) {
-  const prompt = args.join(' ');
-  if (!box) {
-    return api.sendMessage(`Unsupported.`, event.threadID);
-  }
+module.exports.run = async function ({ api, event, args }) {
+		const question = args.join(" ");
+		if (!question) return api.sendMessage("Please provide a question.", event.threadID);
 
-  try {
-    // Available Models: "https://metoushela-rest-api-tp5g.onrender.com/api/gpt4o?"
-    if (!prompt) {
-      box.reply('Please specify a message!');
-      box.react('❓');
-    } else {
-      const info = await box.reply(`Fetching answer...`);
-      box.react('⏱️');
-      const response = await herc.question({ model: 'v3', content: prompt });
-      await box.edit(response.reply, info.messageID);
-      box.react('');
-    }
-  } catch (error) {
-    box.reply('⚠️ Something went wrong: ' + error);
-    box.react('⚠️');
-  }
+		rsnchat.gpt(question).then((response) => {
+				api.sendMessage(response.message, event.threadID);
+		}).catch((error) => {
+				api.sendMessage("An error occurred while processing your request.", event.threadID);
+				console.error(error);
+		});
 };

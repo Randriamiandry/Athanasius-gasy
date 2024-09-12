7bfa7634faecb7ffc835891085b3b7a432f67474
Randriamiandry <randriamiandryathanasios@gmail.com>
2024-09-12 12:07:48 +0300

    Update help.js

--- script/help.js
+++ script/help.js
@@ -33,7 +33,7 @@ module.exports.run = async function({
       eventCommands.forEach((eventCommand, index) => {
         helpMessage += `\t${index + 1}. 「 ${prefix}${eventCommand} 」\n`;
       });
-      helpMessage += `\nPage ${page}/${Math.ceil(commands.length / pages)}. Pour afficher la page suivante, tapez '${prefix}help numéro de page'. L'objectif d'ATH BOT est de venir en aide à toute personne nécessitant une assistance.`;
+      helpMessage += `\nPage ${page}/${Math.ceil(commands.length / pages)}. Pour afficher la page suivante, tapez '${prefix}help numéro de page'. L'objectif est de venir en aide à toute personne nécessitant une assistance.`;
       api.sendMessage(helpMessage, event.threadID, event.messageID);
     } else if (!isNaN(input)) {
       const page = parseInt(input);
